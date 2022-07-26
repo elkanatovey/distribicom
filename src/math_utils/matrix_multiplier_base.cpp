3d23f5ed5b24f975380c1861c97771947fa107ec
elkanatovey <elkanatovey@gmail.com>
2022-07-26 13:38:33 +0300

    multiplication_utils: start refactor of multiplication logic

---
+++ src/math_utils/matrix_multiplier_base.cpp
@@ -0,0 +1,3 @@
+#include "matrix_multiplier_base.hpp"
+
+namespace multiplication_utils{}
---
+++ src/math_utils/matrix_multiplier_base.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "pir.hpp"
+#include "pir_client.hpp"
+
+namespace multiplication_utils{
+    class matrix_multiplier_base {
+
+    };
+}
