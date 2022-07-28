37a97671933da64c589590a5a0baba34cf693f23
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2022-07-28 14:49:12 +0300

    multiplication utils unit test

--- test/multiplication_utils_test.cpp
+++ test/multiplication_utils_test.cpp
@@ -19,8 +19,8 @@ int main(int argc, char *argv[]) {
 
     cout << "Main: generating matrices" << endl;
 
-    auto rows = 20;
-    auto cols = 4;
+    std::uint64_t rows = 20;
+    std::uint64_t cols = 4;
     std::vector<seal::Plaintext> matrix(rows * cols);
 
     std::vector<std::uint64_t> random_values(all.encryption_params.poly_modulus_degree(), 0ULL);
@@ -31,5 +31,11 @@ int main(int argc, char *argv[]) {
 
 
     auto matrix_multiplier = multiplication_utils::matrix_multiplier::Create(all.evaluator, all.encryption_params);
+
+    std::vector<std::uint64_t> left_vec(rows, 1);
+    std::vector<seal::Plaintext> results(cols, seal::Plaintext(all.encryption_params.poly_modulus_degree()));
+
+    std::vector<std::uint64_t> dims = {cols, rows};
+    matrix_multiplier->left_multiply(dims, left_vec, matrix, results);
 }
 
