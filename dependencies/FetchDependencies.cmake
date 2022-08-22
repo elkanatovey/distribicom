0a13ae3c4b5f13ff5bda88b3ff8caf10e1bae7be
elkanatovey <elkanatovey@gmail.com>
2022-08-22 22:07:25 +0300

    dependdencies: move location of seal download

--- dependencies/FetchDependencies.cmake
+++ dependencies/FetchDependencies.cmake
@@ -4,12 +4,12 @@ message("seal dir is ${SEAL_DIR}")
 if((NOT SEAL_DIR) OR GET_SEAL)
     message("Getting dependencies... ")
     # get dependencies (just seal for now)
-    configure_file(dependencies/FetchSeal.cmake seal_download/CMakeLists.txt)
+    configure_file(dependencies/FetchSeal.cmake CMakeLists.txt)
 
     # get seal
     execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
             RESULT_VARIABLE result
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/seal_download )
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} )
     if(result)
         message(FATAL_ERROR "CMake step for seal failed: ${result}")
     endif()
@@ -17,13 +17,15 @@ if((NOT SEAL_DIR) OR GET_SEAL)
     # build seal
     execute_process(COMMAND ${CMAKE_COMMAND} --build .
             RESULT_VARIABLE result
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/seal_download )
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} )
     if(result)
         message(FATAL_ERROR "Build step for seal failed: ${result}")
     endif()
-    set(SEAL_DIR ${CMAKE_BINARY_DIR}/seal_download/_deps/com_microsoft_seal-build/cmake CACHE STRING "" FORCE)
+    set(SEAL_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/com_microsoft_seal-build/cmake CACHE STRING "" FORCE)
     message("seal dir is ${SEAL_DIR}")
+    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/CMakeLists.txt)
 endif()
+find_package(SEAL 4.0.0 REQUIRED PATH ${SEAL_DIR})
 
 message("seal dir is ${SEAL_DIR}")
 set(SEAL_DIR ${SEAL_DIR} CACHE STRING "" FORCE)
