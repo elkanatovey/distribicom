36e346f643b198beab216002c3e4ed3e202f00c5
elkanatovey <elkanatovey@gmail.com>
2022-08-23 13:41:24 +0300

    cmake: stop auto generation of dependency targets

--- dependencies/FetchGRPC.cmake
+++ dependencies/FetchGRPC.cmake
@@ -3,12 +3,19 @@ FetchContent_Declare(
         gRPC
         GIT_REPOSITORY https://github.com/grpc/grpc
         GIT_TAG        v1.48.0  # e.g v1.28.0
+        USES_TERMINAL_DOWNLOAD ON
 )
 set(FETCHCONTENT_QUIET OFF)
 
 if(NOT gRPC_POPULATED)
-    FetchContent_MakeAvailable(gRPC)
+    FetchContent_Populate(gRPC)
+    add_subdirectory(
+            ${grpc_SOURCE_DIR}
+            ${grpc_BINARY_DIR}
+            EXCLUDE_FROM_ALL)
 endif()
+
+
 # Since FetchContent uses add_subdirectory under the hood, we can use
 # the grpc targets directly from this build.
 set(_PROTOBUF_LIBPROTOBUF libprotobuf)
