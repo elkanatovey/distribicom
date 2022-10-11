c1f76de60ac0cb6e4fa07c97e339396a0e798912
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2022-10-11 08:08:04 +0300

    adding channel and waitgroup

--- src/math_utils/channel.h
+++ src/math_utils/channel.h
@@ -24,6 +24,12 @@ public:
         close();
     }
 
+    // not allowing copy or moving of a channel.
+    Channel(const Channel &) = delete;
+
+    Channel(Channel &&) = delete;
+
+
     // Add an element to the queue.
     void write(T t) {
         std::lock_guard<std::mutex> lock(m);
@@ -56,3 +62,41 @@ private:
     mutable std::mutex m;
     std::condition_variable c;
 };
+
+class WaitGroup {
+private:
+    std::mutex m;
+    std::condition_variable cv;
+    int count = 0;
+public:
+    WaitGroup() : m(), cv() {}
+
+    // not allowing move or copy.
+    WaitGroup(const WaitGroup &) = delete;
+
+    WaitGroup(WaitGroup &&) = delete;
+
+    // todo: can be performed without locking though, just atomic ops...
+    void add(int delta) {
+        std::lock_guard<std::mutex> lock(m);
+        count += delta;
+    }
+
+    void done() {
+        std::lock_guard<std::mutex> lock(m);
+        count -= 1;
+        if (count == 0) {
+            cv.notify_all();
+            return;
+        }
+        if (count < 0) {
+            throw std::runtime_error("negative counter in wait group!");
+        }
+    }
+
+    void wait() {
+        std::unique_lock<std::mutex> lock(m);
+        cv.wait(lock, [this] { return count == 0; });
+    }
+};
+
