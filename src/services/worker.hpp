53b614c82a4900890ab988cfc5c10767a709f0a7
Jonathan Weiss <jonathan.weiss@algorand.com>
2022-10-14 13:14:24 +0300

    fx: including services in the cmake of test

--- src/services/worker.hpp
+++ src/services/worker.hpp
@@ -3,7 +3,15 @@
 #include "distribicom.pb.h"
 #include "distribicom.grpc.pb.h"
 
-class Worker final : public distribicom::Worker::Service{
+class Worker final : public distribicom::Worker::Service {
+    ::grpc::Status SendTasks(::grpc::ServerContext *context, ::grpc::ServerReader<::distribicom::MatrixPart> *reader,
+                             ::distribicom::Ack *response) override {
+        // TODO: secure this stream.
+        distribicom::MatrixPart tmp;
+        while (reader->Read(&tmp)) {
+            std::cout << "We are live." << std::endl;
+        }
+    }
 
 };
 
--- test/services/worker_test.cpp
+++ test/services/worker_test.cpp
@@ -1,4 +1,5 @@
 #include "../test_utils.hpp"
+#include "worker.hpp"
 
 int worker_test(int, char *[]) {
     return 0;
