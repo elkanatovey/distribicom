bde3b8a3a5ac0b98c388aac5c8cc2ba4563d31c1
Jonathan Weiss <jonathan.weiss@algorand.com>
2022-10-16 10:16:47 +0300

    testing connection to basic service

--- test/services/worker_test.cpp
+++ test/services/worker_test.cpp
@@ -1,6 +1,54 @@
 #include "../test_utils.hpp"
 #include "worker.hpp"
+#include <grpc++/grpc++.h>
+
+
+std::unique_ptr<grpc::Server> run_server();
 
 int worker_test(int, char *[]) {
+    WaitGroup wg;
+
+    wg.add(1);
+    std::thread t([&] {
+        std::string server_address("0.0.0.0:50051");
+        services::Worker service;
+
+        grpc::ServerBuilder builder;
+        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+        builder.RegisterService(&service);
+        auto server(builder.BuildAndStart());
+        std::cout << "Server listening on " << server_address << std::endl;
+        std::cout << "\n\n" << std::endl;
+        wg.wait();
+        server->Shutdown();
+    });
+    std::this_thread::sleep_for(std::chrono::milliseconds(2 * 000));
+
+    distribicom::Worker::Stub client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+    grpc::ClientContext context;
+    distribicom::Ack response;
+
+    distribicom::MatrixPart m;
+
+    context.AddMetadata("client", "abc");
+    auto conn = client.SendTasks(&context, &response);
+
+    conn->WriteLast(m, grpc::WriteOptions{});
+    wg.done();
+    t.join();
     return 0;
+}
+
+std::unique_ptr<grpc::Server> run_server() {
+    std::string server_address("0.0.0.0:50051");
+    services::Worker service;
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    auto server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    std::cout << "\n\n" << std::endl;
+    server->Wait();
+    return nullptr;
 }
