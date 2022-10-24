bfeb1979d7af8c7231ded67208e231b9cb88927a
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2022-10-24 09:51:37 +0300

    fx build

---
+++ src/services/constants.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace services::constants {
+    typedef metadata string;
+    const metadata size_md = "m_size";
+    const metadata round_md = "round";
+    const metadata epoch_md = "epoch";
+}
