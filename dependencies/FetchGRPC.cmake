191d3880cc3c366181591a297e292e2874a3599b
Elkana Tovey <40407298+elkanatovey@users.noreply.github.com>
2022-10-31 18:20:00 +0200

    Minor maintainence (#22)
    * FetchDependencies.cmake: add option for precompiled libraries
    
    * README: update readme for new compile options
    
    * grpc: update grpc version
    
    * .github: create caching version of test

--- dependencies/FetchGRPC.cmake
+++ dependencies/FetchGRPC.cmake
@@ -1,29 +1,58 @@
-include(FetchContent)
-FetchContent_Declare(
-        gRPC
-        GIT_REPOSITORY https://github.com/grpc/grpc
-        GIT_TAG        v1.48.0  # e.g v1.28.0
-        USES_TERMINAL_DOWNLOAD ON
-)
-set(FETCHCONTENT_QUIET OFF)
 
-if(NOT gRPC_POPULATED)
-    FetchContent_Populate(gRPC)
-    add_subdirectory(
-            ${grpc_SOURCE_DIR}
-            ${grpc_BINARY_DIR}
-            EXCLUDE_FROM_ALL)
-endif()
+if(USE_PREINSTALLED_GRPC)
+    # Find previously-installed gRPC
+    find_package(Protobuf CONFIG REQUIRED)
 
+    message(STATUS "Using protobuf ${Protobuf_VERSION}")
 
-# Since FetchContent uses add_subdirectory under the hood, we can use
-# the grpc targets directly from this build.
-set(_PROTOBUF_LIBPROTOBUF libprotobuf)
-set(_REFLECTION grpc++_reflection)
-set(_PROTOBUF_PROTOC $<TARGET_FILE:protoc>)
-set(_GRPC_GRPCPP grpc++)
-if(CMAKE_CROSSCOMPILING)
-    find_program(_GRPC_CPP_PLUGIN_EXECUTABLE grpc_cpp_plugin)
+    set(_PROTOBUF_LIBPROTOBUF protobuf::libprotobuf)
+    set(_REFLECTION gRPC::grpc++_reflection)
+    if(CMAKE_CROSSCOMPILING)
+        find_program(_PROTOBUF_PROTOC protoc)
+    else()
+        set(_PROTOBUF_PROTOC $<TARGET_FILE:protobuf::protoc>)
+    endif()
+
+    # Find gRPC installation
+    # Looks for gRPCConfig.cmake file installed by gRPC's cmake installation.
+    find_package(gRPC CONFIG REQUIRED)
+    message(STATUS "Using gRPC ${gRPC_VERSION}")
+
+    set(_GRPC_GRPCPP gRPC::grpc++)
+    if(CMAKE_CROSSCOMPILING)
+        find_program(_GRPC_CPP_PLUGIN_EXECUTABLE grpc_cpp_plugin)
+    else()
+        set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:gRPC::grpc_cpp_plugin>)
+    endif()
+
 else()
-    set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:grpc_cpp_plugin>)
+    include(FetchContent)
+    FetchContent_Declare(
+            gRPC
+            GIT_REPOSITORY https://github.com/grpc/grpc
+            GIT_TAG        v1.50.1  # e.g v1.28.0
+            USES_TERMINAL_DOWNLOAD ON
+    )
+    set(FETCHCONTENT_QUIET OFF)
+
+    if(NOT gRPC_POPULATED)
+        FetchContent_Populate(gRPC)
+        add_subdirectory(
+                ${grpc_SOURCE_DIR}
+                ${grpc_BINARY_DIR}
+                EXCLUDE_FROM_ALL)
+    endif()
+
+
+    # Since FetchContent uses add_subdirectory under the hood, we can use
+    # the grpc targets directly from this build.
+    set(_PROTOBUF_LIBPROTOBUF libprotobuf)
+    set(_REFLECTION grpc++_reflection)
+    set(_PROTOBUF_PROTOC $<TARGET_FILE:protoc>)
+    set(_GRPC_GRPCPP grpc++)
+    if(CMAKE_CROSSCOMPILING)
+        find_program(_GRPC_CPP_PLUGIN_EXECUTABLE grpc_cpp_plugin)
+    else()
+        set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:grpc_cpp_plugin>)
+    endif()
 endif()
