d6c23bfdfa18ceb301bfbb58253dc83c74218c21
elkanatovey <elkanatovey@gmail.com>
2022-11-14 11:06:19 +0200

    client_service: start functions

--- src/services/client_service.cpp
+++ src/services/client_service.cpp
@@ -1,4 +1,18 @@
 #include "client_service.hpp"
 
 namespace services {
+    ClientListener::ClientListener(distribicom::AppConfigs &app_configs) {
+
+    }
+
+    grpc::Status ClientListener::Answer(grpc::ServerContext *context, const distribicom::Ciphertext *answer,
+                                        distribicom::Ack *response) {
+        return Service::Answer(context, answer, response);
+    }
+
+    grpc::Status
+    ClientListener::TellNewRound(grpc::ServerContext *context, const distribicom::TellNewRoundRequest *request,
+                                 distribicom::Ack *response) {
+        return Service::TellNewRound(context, request, response);
+    }
 } // services
--- src/services/client_service.hpp
+++ src/services/client_service.hpp
@@ -1,10 +1,25 @@
 #pragma once
 
 #include "distribicom.grpc.pb.h"
+#include "marshal.hpp"
 
 namespace services {
 
-    class client_service final : public distribicom::Client::Service {
+    // listener run by query owner to receive answer
+    class ClientListener final : public distribicom::Client::Service {
+
+        distribicom::AppConfigs app_configs;
+        std::shared_ptr<marshal::Marshaller> mrshl;
+
+
+        explicit ClientListener(distribicom::AppConfigs &app_configs);
+
+        grpc::Status
+        Answer(grpc::ServerContext *context, const distribicom::Ciphertext *answer,
+               distribicom::Ack *response)override;
+
+        grpc::Status
+        TellNewRound(grpc::ServerContext *context, const distribicom::TellNewRoundRequest* request, distribicom::Ack* response)override;
 
     };
 
