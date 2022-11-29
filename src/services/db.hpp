44a47cfd4ccaeeed2b9b4060d3d782b3fdaa23a9
elkanatovey <elkanatovey@gmail.com>
2022-11-29 16:53:02 +0200

    server_service: start work on db modifying ptxs for between round work

--- src/services/db.hpp
+++ src/services/db.hpp
@@ -78,20 +78,7 @@ namespace pir_primitives{
         };
     };
 
-    template<>
-    void DB<seal::Plaintext>::write(const vector<uint64_t> &new_element, const int ptx_num, uint64_t offset,
-                                       PIRClient replacer_machine) {
-        mtx.lock();
-        memory_matrix.data[ptx_num] = replacer_machine.replace_element(memory_matrix.data[ptx_num], new_element,
-                                                                       offset);
-        mtx.unlock();
-    }
 
-    template<typename T>
-    void
-    DB<T>::write(const vector<uint64_t> &new_element, const int ptx_num, uint64_t offset, PIRClient replacer_machine) {
-        throw std::runtime_error("unsupported type");
-    }
 
 };
 
