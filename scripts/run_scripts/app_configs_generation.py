a0ac234eebc0ae3271e264e31f33e9c3c8b51dab
Jonathan Weiss <jonathan.weiss@algorand.com>
2022-12-25 07:19:58 +0200

    app-conf creation for debugging.

---
+++ scripts/run_scripts/app_configs_generation.py
@@ -0,0 +1,16 @@
+from run import *
+
+"""
+    Used for debugging on localhost.
+"""
+if __name__ == '__main__':
+    args = command_line_args()
+
+    settings = Settings(args.config_file)
+
+    hostname = subprocess.run(['hostname'], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+
+    app_configs = create_app_configs(settings.configs, hostname)
+    with open(settings.app_configs_filename, 'wb') as f:
+        f.write(app_configs.SerializeToString())
+        print(f"configs written to {settings.app_configs_filename}")
