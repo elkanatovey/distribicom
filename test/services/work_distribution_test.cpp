72b0633441e84f9b10bb821315e5c383edd289e5
Jonathan Weiss <jonathan.weiss@algorand.com>
2022-12-26 10:39:00 +0200

    work-distribution test: ensure each group covers the full db. ensures each worker has the same set of queries.

--- test/services/work_distribution_test.cpp
+++ test/services/work_distribution_test.cpp
@@ -9,6 +9,16 @@
 std::map<uint32_t, std::unique_ptr<services::ClientInfo>>
 create_client_db(int size, std::shared_ptr<TestUtils::CryptoObjects> &all, const distribicom::AppConfigs &app_configs);
 
+void verify_partitions_hold_the_same_amount_of_work(map<string, services::WorkerInfo> &partition);
+
+std::vector<services::WorkerInfo>
+split_partitions_by_group(map<string, services::WorkerInfo> &partitions, uint64_t grp_id);
+
+std::set<std::uint64_t> get_group_ids(map<string, services::WorkerInfo> &partitions);
+
+void verify_workers_in_group_contain_the_full_db(map<string, services::WorkerInfo> &partitions,
+                                                 distribicom::AppConfigs &app_cfgs);
+
 int work_distribution_test(int, char *[]) {
     auto all = TestUtils::setup(TestUtils::DEFAULT_SETUP_CONFIGS);
 
@@ -37,13 +47,81 @@ int work_distribution_test(int, char *[]) {
         manager.work_streams[std::to_string(i)] = nullptr;
     }
 
-    manager.map_workers_to_responsibilities2(num_queries);
+    auto partitions = manager.map_workers_to_responsibilities2(num_queries);
 
     /* todo:
-         verify each group has the same number of queries.
-         verify each group holds all DB.
          verify each group has different workers.
          verify each group has different queries.
+         verify all groups together have all queries.
      */
+
+    verify_partitions_hold_the_same_amount_of_work(partitions);
+    verify_workers_in_group_contain_the_full_db(partitions, cfgs);
     return 0;
+}
+
+void
+verify_workers_in_group_contain_the_full_db(map<string, services::WorkerInfo> &partitions,
+                                            distribicom::AppConfigs &app_cfgs) {
+    auto grp_ids = get_group_ids(partitions);
+    for (const auto &grp_id: grp_ids) {
+        auto splits = split_partitions_by_group(partitions, grp_id);
+        if (splits.empty()) {
+            throw std::runtime_error("no splits for group");
+        }
+
+        std::set<uint64_t> db_rows;
+        for (std::uint64_t i = 0; i < app_cfgs.configs().db_rows(); ++i) {
+            db_rows.insert(i);
+        }
+
+        for (const auto &worker_info: splits) {
+            for (const auto &row: worker_info.db_rows) {
+                assert(db_rows.find(row) != db_rows.end()); // ensure row presents.s
+                db_rows.erase(row);
+            }
+        }
+        assert(db_rows.empty()); // ensures each group covers the DB fully.
+    }
+}
+
+std::set<std::uint64_t> get_group_ids(map<string, services::WorkerInfo> &partitions) {
+    std::set<std::uint64_t> group_ids;
+    for (const auto &[nm, info]: partitions) {
+        (void) nm;
+        group_ids.insert(info.group_number);
+    }
+    return group_ids;
+}
+
+std::vector<services::WorkerInfo>
+split_partitions_by_group(map<string, services::WorkerInfo> &partitions, uint64_t grp_id) {
+    std::vector<services::WorkerInfo> splits;
+    for (const auto &[nm, info]: partitions) {
+        (void) nm;
+
+        if (info.group_number != grp_id) {
+            continue;
+        }
+
+        splits.push_back(info);
+    }
+    return splits;
+}
+
+void verify_partitions_hold_the_same_amount_of_work(map<string, services::WorkerInfo> &partition) {
+    auto grp_ids = get_group_ids(partition);
+    for (const auto &grp_id: grp_ids) {
+        auto splits = split_partitions_by_group(partition, grp_id);
+        if (splits.empty()) {
+            throw std::runtime_error("no splits for group");
+        }
+
+        auto query_range_start = splits[0].query_range_start;
+        auto query_range_end = splits[0].query_range_end;
+        for (const auto &worker_info: splits) {
+            assert(worker_info.query_range_start == query_range_start);
+            assert(worker_info.query_range_end == query_range_end);
+        }
+    }
 }
