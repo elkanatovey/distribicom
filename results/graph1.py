0b8d26de1897e589cdb6d5363a528f45a49a61c6
Jonathan Weiss <jonathan.weiss@algorand.com>
2023-01-17 16:13:47 +0200

    tidy graph1.py

--- results/graph1.py
+++ results/graph1.py
@@ -1,5 +1,3 @@
-# TODO:
-# 1. parse the files. and collect their name as data.
 import os
 from typing import List
 
@@ -22,17 +20,10 @@ class SealPirResults:
         self.std = np.std(times)
 
 
-"""
-query processing time: 3341 ms on 126 queries and 12 threads
-Main: pool query processing time: 3345 ms on 126 queries and 12 threads
-Main: pool query processing time: 3354 ms on 126 queries and 12 threads
-Main: pool query processing time: 3359 ms on 126 queries and 12 threads
-Main: pool query processing time: 3371 ms on 126 queries and 12 threads
-"""
 # according to Elkana's collected results.
 sealpir_results_42_queries = SealPirResults(42, [1519, 1476, 1492, 1577, 1517])
 sealpir_results_84_queries = SealPirResults(84, [2737, 2626, 2575, 2643, 2619])
-sealpir_results_126_queries = SealPirResults(126, [3341, 3345, 3354, 3359, 3371])  # No results.
+sealpir_results_126_queries = SealPirResults(126, [3341, 3345, 3354, 3359, 3371])
 sealpir_results_168_queries = SealPirResults(168, [5114, 5053, 4941, 4767, 5041])
 
 
@@ -42,6 +33,7 @@ class TestResult:
         return not any(word in file_name for word in ["slurm", "ignore"])
 
     def __init__(self, file_name: str):
+
         self.file_name = file_name
         # general info.
         self.num_threads_per_worker = 1
@@ -51,64 +43,64 @@ class TestResult:
 
         self.num_workers = int(file_name.split("_")[2])
         self.num_queries = self.num_workers
+
+        self.data = []  # initialise
         self.parse_file()
 
     def parse_file(self):
         with open(self.file_name, "r") as f:
-            for line in f:
-                if "results:" not in line:
-                    continue
+            self.move_seeker_to_results(f)
+            self.collect_data(f)
+
+    def move_seeker_to_results(self, f):
+        for line in f:
+            if "results:" not in line:
+                continue
+            break
+
+    def collect_data(self, f):
+        f.readline()  # skip the first line ( "[" ).
+        self.data = []
+        for line in f:
+            if "]" in line:
                 break
+            self.data.append(line[:-2].strip())
 
-            f.readline()  # skip the first line.
-            self.data = []
-            for line in f:
-                if "]" in line:
-                    break
-                self.data.append(line[:-2].strip())
-
-            self.data = [int(x[:-3]) for x in self.data]  # remove "ms," from each data point
+        self.data = [int(x[:-3]) for x in self.data]  # remove "ms," from each data point
 
 
 def plot_dpir_line(ax, test_results: List[TestResult]):
     test_results = sorted(test_results, key=lambda x: x.num_queries)
-    for test_result in test_results:
-        print(test_result.num_queries, test_result.data)
+
     xs = [0, *(test_result.num_queries for test_result in test_results)]
     ys = [0, *(np.average(test_result.data[1:]) for test_result in test_results)]
 
-    # TODO: verify err_bars are correct.
     errbars = [0, *(np.std(test_result.data[1:]) for test_result in test_results)]
-    print(errbars)
-    # y is the avg of test_result.data
-    # TODO: colour nicely.
     ax.plot(xs, ys, marker='o', color=dpir_clr, linewidth=line_size, markersize=line_size + 1)
     ax.errorbar(xs, ys, yerr=errbars, fmt='.', markersize=4, barsabove=True, capsize=2, ecolor=dpir_clr, color=dpir_clr,
                 elinewidth=line_size - 1)
 
 
 def plot_sealpir_line(ax):
-    xs = [0,
-          42,
-          84,
-          126,
-          168]
-    ys = [
-        sealpir_results_42_queries,
-        sealpir_results_84_queries,
-        sealpir_results_126_queries,
-        sealpir_results_168_queries
-    ]
-    errbars = [0, *map(lambda x: x.std, ys)]
+    ys = sorted(
+        [
+            sealpir_results_42_queries,
+            sealpir_results_84_queries,
+            sealpir_results_126_queries,
+            sealpir_results_168_queries
+        ],
+        key=lambda x: x.queries,
+    )
+
+    xs = [0, *(result.queries for result in ys)]
+    errbars = [0, *map(lambda y: y.std, ys)]
     print(errbars)
-    ys = [0, *map(lambda x: x.avg, ys)]
+    ys = [0, *map(lambda y: y.avg, ys)]
     ax.plot(xs, ys, color=sealpir_clr, marker='o', linewidth=line_size, markersize=line_size + 1)
     ax.errorbar(xs, ys, yerr=errbars, fmt='.', barsabove=True, capsize=2, ecolor=sealpir_clr, color=sealpir_clr)
 
 
-# colour-pallet: https://coolors.co/443d4a-55434e-ba6567-fe5f55-e3a792
-if __name__ == '__main__':
-    folder_path = "./1thread"
+def collect_test_results(folder_path):
     test_results = []
     for filename in os.listdir(folder_path):
         if not TestResult.is_test_result(filename):
@@ -118,6 +110,12 @@ if __name__ == '__main__':
 
     if len(test_results) == 0:
         raise Exception("No test results found.")
+    return test_results
+
+
+# colour-pallet: https://coolors.co/443d4a-55434e-ba6567-fe5f55-e3a792
+if __name__ == '__main__':
+    test_results = collect_test_results("./1thread")
 
     fig, ax = plt.subplots()
     plot_dpir_line(ax, test_results)
@@ -127,6 +125,7 @@ if __name__ == '__main__':
 
     ax.set_xticks([0, 42, 84, 126, 168])
     ax.set_yticklabels(map(lambda x: str(x) + "s", [0, 0, 1, 2, 3, 4, 5]))
+
     ax.set_xlabel('number of queries')
     ax.set_ylabel('round latency')
 
