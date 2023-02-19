73574199d591ac3826798c31017f2a1ffdd0e66a
elkanatovey <elkanatovey@gmail.com>
2023-02-19 19:12:27 +0200

    README: write bench for 65k size db

--- scripts/run_scripts/benchmark_scripts/check_server.sh
+++ scripts/run_scripts/benchmark_scripts/check_server.sh
@@ -1,20 +1,75 @@
 #!/bin/bash
 
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=50
 #SBATCH --cpus-per-task=2
-#SBATCH --nodes=5
-#SBATCH --time=00:10:00
-#SBATCH --mem=64G
+#SBATCH --nodes=11
+#SBATCH --time=04:10:00
+#SBATCH --mem=128G
 
 
 #SBATCH hetjob
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=24
 #SBATCH --nodes=1
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 
 
 
-srun -n 5 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 30 5 12
+srun -n 42 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 42 42 12
+
+srun -n 84 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 84 84 12
+
+srun -n 126 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 126 126 12
+
+srun -n 168 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 168 168 12
+
+srun -n 210 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 210 210 12
+
+
+srun -n 252 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 252 252 12
+
+
+srun -n 294 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 294 294 12
+
+srun -n 336 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 336 336 12
+
+
+srun -n 378 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 378 378 12
+
+srun -n 420 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 420 420 12
+
+srun -n 462 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 462 462 12
+
+
+
+srun -n 504 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 504 504 12
+
+srun -n 546 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 546 546 12
+
+srun -n 588 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 588 588 12
+
+srun -n 630 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 630 630 12
+
+srun -n 672 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 672 672 12
+
+srun -n 714 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 714 714 12
+
+srun -n 756 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 756 756 12
+
+srun -n 798 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 798 798 12
+
+srun -n 840 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 840 840 12
+
+srun -n 882 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 882 882 12
+
+
+srun -n 924 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 924 924 12
+
+srun -n 966 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 966 966 12
+
+srun -n 1008 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 1008 1008 12
+
+srun -n 1050 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 1050 1050 12
+
 
 srun -n 5 hostname : -n 1 hostname
