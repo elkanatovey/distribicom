71e3c182ec9c66a1566132f5cadd7cced44ac685
mcboomy <jonathan.weiss1@mail.huji.ac.il>
2023-03-25 10:12:36 +0300

    server writes specific ip

--- scripts/run_scripts/benchmark_scripts/scripts_mil_size/small_server.sh
+++ scripts/run_scripts/benchmark_scripts/scripts_mil_size/small_server.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --ntasks-per-node=32
 #SBATCH --cpus-per-task=2
-#SBATCH --nodes=8
+#SBATCH --nodes=2
 #SBATCH --time=04:10:00
 #SBATCH --mem=800G
 
@@ -15,8 +15,8 @@
 
 
 
-
-
-srun -n 164 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 164 164 12
+srun -n 41 python3 run_worker.py location_configs.json pir_configs.json 2 : -n 1 python3 run_server.py location_configs.json pir_configs.json 41 41 12
 
 srun -n 5 hostname : -n 1 hostname
+
+
