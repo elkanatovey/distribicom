a3cb2cc912fe05b819e0d0dc3790d5faaf74202c
Jonathan Weiss <jonathanweiss@huji-132-64-29-63.xt.huji.ac.il>
2023-04-01 09:11:36 +0300

    fourth graph plotting

---
+++ results/graph4.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+import utils, constants
+
+# x db size to times.
+dpir_values = {
+    10: [28967, 28860, 28715, 28456, 28427, 29059, 27729, 28211, 28438],
+    20: [19962, 18139, 19332, 18147, 17787, 18450, 18698, 17859, 17848],
+    40: [14503, 14201, 14288, 14208, 14250, 14129],
+    60: [13793, 13806, 13760, 13585, 13783, 13650, 13922, 14133, 13772],
+    80: [14158, 13935, 13912, 14026, 14101, 13750, 14152, 14073, 13944],
+    100: [13989, 13855, 13735, 13882, 13867, 13951, 13826, 13741, 13815]
+}
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+
+    xs = sorted(dpir_values.keys())
+    ys = [np.mean(dpir_values[x]) for x in xs]
+    yerr = [np.std(dpir_values[x]) for x in xs]
+
+    utils.add_y_format(ax)
+
+    clr = constants.dpir_clr
+    pr = {
+        "markersize": 4,
+        "barsabove": True,
+        "capsize": 2,
+        "elinewidth": constants.line_size - 1,
+        "ecolor": clr,
+        "color": clr,
+    }
+
+    ax.errorbar(xs, ys, yerr=yerr, **pr)
+    ax.set_xlabel("mbit/s")
+    # ax. = "Number of queries"
+
+    plt.show()
