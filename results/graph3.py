2f278a129dc4637e95e9bd5232544d733d117483
Jonathan Weiss <jonathanweiss@huji-132-64-29-66.xt.huji.ac.il>
2023-04-02 09:30:40 +0300

    graph-3fx

--- results/graph3.py
+++ results/graph3.py
@@ -7,10 +7,16 @@ import utils, constants
 dpir_values = {
     1 << 13: utils.GenericDataPoint(60, [470, 453, 544, 551, 517, 523, 488, 470, 468]),
     1 << 14: utils.GenericDataPoint(105, [1286, 1273, 1267, 1263, 1176, 1180, 1030, 1291, 1255]),
-    1 << 15: utils.GenericDataPoint(210, [4380, 4594, 4383, 4319, 4377, 3974, 4088, 3732, 3557]),
+    1 << 15: utils.GenericDataPoint(210, [3535, 3405, 3401, 3477, 3300, 3406, 3329, 3348]),
     1 << 16: utils.GenericDataPoint(420, [6397, 6410, 6430, 6426, 6426, 6438, 6430, 6424, 6462])
 }
 
+fpir_values = {
+    1 << 13: utils.GenericDataPoint(60, [182, 181, 170, 173, 171, 172]),
+    1 << 14: utils.GenericDataPoint(105, [382, 381, 382, 381, 383]),
+    1 << 15: utils.GenericDataPoint(210, [1071, 1069, 1071, 1065, 1068]),
+    1 << 16: utils.GenericDataPoint(420, [3289, 3297, 3294, 3299, 3280])
+}
 sealpir_values = sealpir_res = utils.grab_sealpir_results_from_file(
     "/Users/jonathanweiss/CLionProjects/distribicom/results/evals/3rd-graph/run_sealpir_log.txt")
 
@@ -57,6 +63,11 @@ if __name__ == '__main__':
     sealpir_errs = [*map(lambda x: sealpir_dict[x].std, xs)]
     set_color(prms, constants.sealpir_clr)
     plt.errorbar(xs, sealpir_ys, sealpir_errs, **prms)
+
+    fpir_ys = [*map(lambda x: fpir_values[x].avg, xs)]
+    fpir_errs = [*map(lambda x: fpir_values[x].std, xs)]
+    set_color(prms, constants.addra_clr)
+    plt.errorbar(xs, fpir_ys, fpir_errs, **prms)
 
     utils.add_y_format(ax)
 
