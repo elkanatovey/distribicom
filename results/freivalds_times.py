55f5fb0bfee9cd1d4389b8e354b79cee10c96a4c
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2023-07-31 16:57:53 +0300

    2mil slurm in results

--- results/freivalds_times.py
+++ results/freivalds_times.py
@@ -21,5 +21,6 @@ if __name__ == '__main__':
         ("65k", "evals/65k_size/32_workers_per_node/third_run/slurm-5816869.out"),
         ("256k", "evals/256k/slurm-6291958.out"),
         ("1m", "evals/scripts_mil_size/slurm-q328-c24.log"),
+        ("2m", "evals/2mil-slurm.out"),
     ]:
         print(f"{db_size}: {np.average(grab_verification_times(fname))} ms")
