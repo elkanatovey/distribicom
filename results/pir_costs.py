6286deda56688790e80f7f27bd13d76a40c91a0b
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2023-08-01 05:16:59 +0300

    fx: pir-costs had wrong num mults in vector dot prod

--- results/pir_costs.py
+++ results/pir_costs.py
@@ -10,7 +10,7 @@ mult_plain_time = 61054
 
 
 def num_ops_in_vec_to_vec_mult(n):
-    return {"mult": 2 * n, "add": n - 1}
+    return {"mult": n, "add": n - 1}
 
 
 def matrix_mult_ops(n, m, k):
@@ -42,39 +42,9 @@ def ns2ms(ns):
 
 if __name__ == '__main__':
     # db sizes to PIR costs
-    pir1_ys = []
-    pir2_ys = []
-
-    xs = []
     for rows, cols in [(42, 41), (82, 82), (164, 164), (232, 232)]:
         pir1 = pir_query_lvl1_time(rows, cols)
         pir2 = pir_query_lvl2_time(rows, cols)
         db_size = rows * cols * 39
 
         print(f"{db_size} & {ns2ms(pir1)} & {ns2ms(pir2)} \\\\")
-
-        xs.append(db_size)
-        # in seconds:
-        pir1_ys.append(pir1 * 1e-9)
-        pir2_ys.append(pir2 * 1e-9)
-
-    fig, ax = plt.subplots()
-
-    plt.plot(xs, np.array(pir1_ys), label="PIR lvl1")
-    plt.plot(xs, np.array(pir2_ys), label="PIR lvl2")
-
-
-    #
-    def y_fmt(y, _):
-        return '{:.0f}s'.format(y)
-
-
-    ax.yaxis.set_major_formatter(ticker.FuncFormatter(y_fmt))
-
-    ax.set_xticks(xs)
-    ax.set_xticklabels(["$2^{16}$", "$2^{18}$", "$2^{20}$", "$2^{21}$"])
-    ax.set_ylim(0)
-
-    ax.legend()
-    fig.tight_layout()
-    plt.show()
